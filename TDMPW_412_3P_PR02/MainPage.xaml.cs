b4f8e77df24f47333a723d1911c9d45ad718ca8c
diegoal3mx <diegoajm2003@gmail.com>
2023-06-02 22:25:34 -0600

    Agregar archivos de proyecto.

---
+++ TDMPW_412_3P_PR02/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace TDMPW_412_3P_PR02;
+
+public partial class MainPage : ContentPage
+{
+    public List<task> tasks { get; set; } = new List<task>();
+
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+        task test = new task(1, "Terminar Práctica 2", false);
+        tasks.AddRange(new task[] { test });
+        BindingContext = this;
+    }
+    
+	
+}
+
---
+++ TDMPW_412_3P_PR02/task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDMPW_412_3P_PR02
+{
+    public class task
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public bool status { get; set; }
+
+        public task(int id, string description, bool status)
+        {
+            this.id = id;
+            this.description= description;
+            this.status = status;
+        }
+
+    }
+
+
+}
